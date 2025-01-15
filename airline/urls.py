d5bb7acf72f4833807b13ce345222ed00dd01b69
Eric <ericaugust2006@gmail.com>
2025-01-14 23:01:22 -0300

    Initial commit: Airline project with flights and users authentication

--- airline/urls.py
+++ airline/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # aqui fica a parte dos admin a primeira url
     path('admin/', admin.site.urls),
-    path('flights', include('flights.urls')),
+    path('flights/', include('flights.urls')),
+    path('users/', include('users.urls'))
 ]
